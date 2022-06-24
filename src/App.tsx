ba7c6df9a17bf26e54c3bc137616a49375c3f040
victor amaral <victor.amaralb@hotmail.com>
2022-06-24 17:27:10 -0300

    aula 03 update

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import Event from "../src/pages/Event";
 import { client } from "./lib/apollo";
+import { Router } from "./Router";
 
 function App() {
   return (
     <div className="App">
       <ApolloProvider client={client}>
         <BrowserRouter>
-          <Event />
+          <Router />
         </BrowserRouter>
       </ApolloProvider>
     </div>
