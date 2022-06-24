87f84f45e7bbf6f1904fe811e1587d5e38f6c216
victor amaral <victor.amaralb@hotmail.com>
2022-06-24 17:17:46 -0300

    aula 03

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
 import Event from "../src/pages/Event";
+import { client } from "./lib/apollo";
 
 function App() {
   return (
     <div className="App">
-      <Event />
+      <ApolloProvider client={client}>
+        <BrowserRouter>
+          <Event />
+        </BrowserRouter>
+      </ApolloProvider>
     </div>
   );
 }
