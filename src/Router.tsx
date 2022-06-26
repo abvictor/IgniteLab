6fdeb7977741fd79f086f96d985b80bacd202f1c
victor amaral <victor.amaralb@hotmail.com>
2022-06-26 20:26:11 -0300

    aula 4

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Event from "./pages/Event";
+import Subscribe from "./pages/Subscribe";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<h1>Home</h1>} />
-
+      <Route path="/subscribe" element={<Subscribe />} />
       <Route path="/event/lesson/:slug" element={<Event />} />
     </Routes>
   );
